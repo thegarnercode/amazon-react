a48f89acf1b81c828f605fa836a4059ccf9fba67
denisha garner <dgarner101@icloud.com>
2021-05-14 19:56:58 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBJJAArujetbkM-nev6rPNmOqJJksnf7ug",
+    authDomain: "react-67a09.firebaseapp.com",
+    projectId: "react-67a09",
+    storageBucket: "react-67a09.appspot.com",
+    messagingSenderId: "1058441639788",
+    appId: "1:1058441639788:web:13cc92ad6170f0c5212549",
+    measurementId: "G-JMKTHVRG4B"
+  };
+  
